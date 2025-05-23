deced1d6f0aa4f693d84f60ac2be09d47b8a38ef
Jakub Kozlowski <jakub.kozlowski@cujo.com>
2025-05-23 16:10:47 +0200

    fixed testfile

--- examples/quantile_example.c
+++ examples/quantile_example.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
+
 #include "tdigest.h"
 
 #define STREAM_SIZE 1000000
 
 static inline double randMToN(double M, double N)
 {
-    return M + (rand() / (RAND_MAX / (N - M)));
+	//NOLINTNEXTLINE(cert-msc30-c, cert-msc50-cpp)
+	return M + (rand() / (RAND_MAX / (N - M)));
 }
 
-
-int main()
+int main(void)
 {
+	td_histogram_t *mdigest =
+		td_new(500); //NOLINT(readability-magic-numbers)
+	printf("compression is %f capacity is %lu\n",
+	       mdigest->compression,
+	       mdigest->cap);
+	double seeds[STREAM_SIZE];
+	for (int i = 0; i < STREAM_SIZE; ++i) {
+		seeds[i] = randMToN(0, 10);
+	}
 
-    td_histogram_t *mdigest = td_new(500);
-    printf("compression is %f capacity is %d\n", mdigest->compression, mdigest->cap);
-    double seeds[STREAM_SIZE];
-    for (int i = 0; i < STREAM_SIZE; ++i)
-    {
-        seeds[i] = randMToN(0, 10);
-    }
-
-    for (int i = 0; i < STREAM_SIZE; ++i)
-    {
-        td_add(mdigest, seeds[i], 1);
-    }
-    td_compress(mdigest);
-    for (int i = 0; i < 10; ++i)
-    {
-        const double v = seeds[i];
-        printf("value %f is at percentile %f\n", v, td_cdf(mdigest, v));
-    }
-    printf("\n");
-    for (int i = 0; i <= 100; i += 10)
-    {
-        printf("%d percentile has value %f\n", i, td_quantile(mdigest, i / 100.0));
-    }
+	for (int i = 0; i < STREAM_SIZE; ++i) {
+		td_add(mdigest, seeds[i], 1);
+	}
+	td_compress(mdigest);
+	for (int i = 0; i < 10; ++i) {
+		const double v = seeds[i];
+		printf("value %f is at percentile %f\n", v, td_cdf(mdigest, v));
+	}
+	printf("\n");
+	for (int i = 0; i <= 100; i += 10) { //NOLINT(readability-magic-numbers)
+		printf("%d percentile has value %f\n",
+		       i,
+		       td_quantile(mdigest, i / 100.0));
+	}
 }
